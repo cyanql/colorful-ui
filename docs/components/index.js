f2430543283a888bb66c35aa213faa64b95ff2b1
keyves <icarusves@gmail.com>
2017-02-09 02:39:09 +0800

    update menu, ripple, dropdown update docs update styles/reset

---
+++ docs/components/index.js
@@ -0,0 +1,21 @@
+import Button from './button'
+import Modal from './modal'
+import Input from './input'
+import Checkbox from './checkbox'
+import Switch from './switch'
+import Radio from './radio'
+import Popover from './popover'
+import Menu from './menu'
+import Dropdown from './dropdown'
+
+export {
+	Button,
+	Popover,
+	Menu,
+	Dropdown,
+	Modal,
+	Input,
+	Checkbox,
+	Switch,
+	Radio
+}
